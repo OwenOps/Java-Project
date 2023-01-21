d3a74910a2014f5b5ba9c020b3398f20911a7b0d
Owen <rebellerowen@gmail.com>
2023-01-21 17:40:23 +0100

    Pendu

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in); // "System.in" pour entrer clavier
+//        Scanner sc = new Scanner(System.in); // "System.in" pour entrer clavier
 //      String mot = sc.nextLine(); // Le sc.next c'est ou est stocker le mot de l'entree clavier, "next", lire un mot
         Panel pa = new Panel();
 
